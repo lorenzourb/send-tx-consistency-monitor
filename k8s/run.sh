9801413a68cafc2c572e0b118e87b699ac3b412a
Jason Yi <jasonhk.yi@consensys.net>
2022-07-05 11:55:47 +0800

    feat: update pod dashboard
    Signed-off-by: Jason Yi <jasonhk.yi@consensys.net>

--- k8s/run.sh
+++ k8s/run.sh
@@ -49,13 +49,15 @@ if [ "$INIT" == "true" ] || [ "$UPDATE" == "true" ] ; then
   exit 0
 fi
 
+ENV=dev_0
+
 if [ "$RUN" == "true" ]; then
   #tar -C ../ -X ../.gitignore -cpzf - . | kubectl exec -n $NS -i $POD -- tar -xpzf - .;
   tar -C ../ -cpzf - . | kubectl exec -n $NS -i $POD -- tar -xpzf - .;
 
   kubectl exec -n $NS -i $POD  -- /bin/sh -c "date && \
         echo Starting... && \
-          K6_INFLUXDB_PUSH_INTERVAL=2s k6 run scenarios/harmony_kpi.js -e env=dev_0 \
+          K6_INFLUXDB_PUSH_INTERVAL=2s k6 run scenarios/harmony_kpi.js -e env=$ENV \
           --out influxdb=$INFLUXDB && \
         echo Finished..."
 fi
