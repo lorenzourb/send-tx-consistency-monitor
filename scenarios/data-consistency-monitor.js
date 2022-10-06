a73d689b289cbe30dadf4bf052238c479a95deff
Lorenzo Urbini <lorenzo.urbini@consensys.net>
2022-10-06 17:56:29 +0200

    more test changes

--- scenarios/data-consistency-monitor.js
+++ scenarios/data-consistency-monitor.js
@@ -17,50 +17,153 @@ export const options = {
       rate: 15,
       timeUnit: '1s', // 1000 iterations per second, i.e. 1000 RPS
       duration: '24h',
-      preAllocatedVUs: 100, // how large the initial pool of VUs would be
-      maxVUs: 100, // if the preAllocatedVUs are not enough, we can initialize more
+      preAllocatedVUs: 300, // how large the initial pool of VUs would be
+      maxVUs: 300, // if the preAllocatedVUs are not enough, we can initialize more
     },
-  }
+  },
 };
 
-export let blockNumberIncreaseErrorRate = new Rate('Block Number Increase Errors');
-// export let erigonErrorRate = new Rate("ErigonErrors");
+export let blockNumberIncreaseErrorRateInfura = new Rate('Block Number Increase Errors Infura');
+export let blockNumberIncreaseErrorRateAlchemy = new Rate('Block Number Increase Errors Alchemy');
+export let blockNumberIncreaseErrorRatePokt = new Rate('Block Number Increase Errors Pokt');
+export let blockNumberIncreaseErrorRateQuicknode = new Rate(
+  'Block Number Increase Errors Quicknode',
+);
+export let blockNumberIncreaseErrorRateCoinbase = new Rate('Block Number Increase Errors Coinbase');
 
-export let blockNumber;
+export let blockNumberInfura = 0;
+export let blockNumberAlchemy = 0;
+export let blockNumberPokt = 0;
+export let blockNumberQuicknode = 0;
+export let blockNumberCoinbase = 0;
+
+export const payload = JSON.stringify(data[Math.floor(Math.random() * data.length)]);
+export const params = {
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: '120s',
+};
 
 export default function () {
-  group('Data consistency blockNumber increase', function () {
+  group('Data consistency blockNumber increase - Infura', function () {
     const url = `https://mainnet.infura.io/v3/b62f751572534b40884d6e18c291fc07`;
-    // const url = `https://mainnet.infura.io/v3/${__ENV.INFURA_KEY}`;
-    const payload = JSON.stringify(data[Math.floor(Math.random() * data.length)]);
-    const params = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      timeout: '120s',
-    };
     const res = http.post(url, payload, params);
-    blockNumber =
-      (blockNumber === 0 && res.body && res.body.result)
-        ? parseInt(JSON.parse(res.body).result, 16)
-        : 0;
+
+    if (blockNumberInfura === 0 && res.body && JSON.parse(res.body).result) {
+      blockNumberInfura = parseInt(JSON.parse(res.body).result, 16);
+    }
+
     let success = check(res, {
       'is status 200': r => r.status === 200,
       'verify rpc resp': r => r.body.includes('"jsonrpc":"2.0"'),
       'verify rpc resp - no err': r => !r.body.includes('error'),
-      'block increase check': r => 
-        parseInt(JSON.parse(r.body).result, 16) - blockNumber == 1  ||  parseInt(JSON.parse(r.body).result, 16) - blockNumber == 0 
+      'block increase check Infura': r =>
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberInfura == 1 ||
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberInfura == 0,
     });
     if (!success) {
       // console.log(res.status === 200);
       // console.log(res.body.includes('"jsonrpc":"2.0"'));
       // console.log(!res.body.includes('error'));
-      console.log(blockNumber);
-      console.log(parseInt(JSON.parse(res.body).result, 16));
-      console.log(parseInt(JSON.parse(res.body).result, 16) >= blockNumber);
-      blockNumberIncreaseErrorRate.add(1);
+      // console.log(blockNumberInfura);
+      // console.log(parseInt(JSON.parse(res.body).result, 16));
+      // console.log(`dedede: ${parseInt(JSON.parse(res.body).result, 16) - blockNumberInfura}`);
+      blockNumberIncreaseErrorRateInfura.add(1);
     } else {
-      blockNumber = parseInt(JSON.parse(res.body).result, 16);
+      blockNumberInfura = parseInt(JSON.parse(res.body).result, 16);
+    }
+  });
+
+  group('Data consistency blockNumber increase - Alchemy', function () {
+    const url = `https://eth-mainnet.g.alchemy.com/v2/jN1FmmT5hBDoTDJTK9fZG0B2tM0ZsR0h`;
+    const res = http.post(url, payload, params);
+
+    if (blockNumberAlchemy === 0 && res.body && JSON.parse(res.body).result) {
+      blockNumberAlchemy = parseInt(JSON.parse(res.body).result, 16);
+    }
+
+    let success = check(res, {
+      'is status 200': r => r.status === 200,
+      'verify rpc resp': r => r.body.includes('"jsonrpc":"2.0"'),
+      'verify rpc resp - no err': r => !r.body.includes('error'),
+      'block increase check Alchemy': r =>
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberAlchemy == 1 ||
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberAlchemy == 0,
+    });
+    if (!success) {
+      blockNumberIncreaseErrorRateAlchemy.add(1);
+    } else {
+      blockNumberAlchemy = parseInt(JSON.parse(res.body).result, 16);
+    }
+  });
+
+  group('Data consistency blockNumber increase - Pokt', function () {
+    const url = `https://eth-rpc.gateway.pokt.network/`;
+    const res = http.post(url, payload, params);
+
+    if (blockNumberPokt === 0 && res.body && JSON.parse(res.body).result) {
+      blockNumberPokt = parseInt(JSON.parse(res.body).result, 16);
+    }
+
+    let success = check(res, {
+      'is status 200': r => r.status === 200,
+      'verify rpc resp': r => r.body.includes('"jsonrpc":"2.0"'),
+      'verify rpc resp - no err': r => !r.body.includes('error'),
+      'block increase check Pokt': r =>
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberPokt == 1 ||
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberPokt == 0,
+    });
+    if (!success) {
+      blockNumberIncreaseErrorRatePokt.add(1);
+    } else {
+      blockNumberPokt = parseInt(JSON.parse(res.body).result, 16);
+    }
+  });
+
+  group('Data consistency blockNumber increase - Quicknode', function () {
+    const url = `https://misty-multi-layer.quiknode.pro/127d957ee03bca11d71663f79b6a4d62f5286c83/`;
+    const res = http.post(url, payload, params);
+
+    if (blockNumberQuicknode === 0 && res.body && JSON.parse(res.body).result) {
+      blockNumberQuicknode = parseInt(JSON.parse(res.body).result, 16);
+    }
+
+    let success = check(res, {
+      'is status 200': r => r.status === 200,
+      'verify rpc resp': r => r.body.includes('"jsonrpc":"2.0"'),
+      'verify rpc resp - no err': r => !r.body.includes('error'),
+      'block increase check Quicknode': r =>
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberQuicknode == 1 ||
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberQuicknode == 0,
+    });
+    if (!success) {
+      blockNumberIncreaseErrorRateQuicknode.add(1);
+    } else {
+      blockNumberQuicknode = parseInt(JSON.parse(res.body).result, 16);
+    }
+  });
+
+  group('Data consistency blockNumber increase - Coinbase', function () {
+    const url = `https://33LUD7E7JUJD4THTSB3J:BW3RMI4RDJXELW6CVWRWNVD6UNHPG32N7G2FUGCX@mainnet.ethereum.coinbasecloud.net/`;
+    const res = http.post(url, payload, params);
+
+    if (blockNumberCoinbase === 0 && res.body && JSON.parse(res.body).result) {
+      blockNumberCoinbase = parseInt(JSON.parse(res.body).result, 16);
+    }
+
+    let success = check(res, {
+      'is status 200': r => r.status === 200,
+      'verify rpc resp': r => r.body.includes('"jsonrpc":"2.0"'),
+      'verify rpc resp - no err': r => !r.body.includes('error'),
+      'block increase check Coinbase': r =>
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberCoinbase == 1 ||
+        parseInt(JSON.parse(r.body).result, 16) - blockNumberCoinbase == 0,
+    });
+    if (!success) {
+      blockNumberIncreaseErrorRateCoinbase.add(1);
+    } else {
+      blockNumberCoinbase = parseInt(JSON.parse(res.body).result, 16);
     }
   });
 }
